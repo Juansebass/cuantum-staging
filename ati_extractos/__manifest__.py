f5eff1d95bc22a5edaf86974788be4f92ea020be
Juansebass <42044743+Juansebass@users.noreply.github.com>
2023-05-29 11:01:42 -0500

    Update __manifest__.py

--- ati_extractos/__manifest__.py
+++ ati_extractos/__manifest__.py
@@ -14,7 +14,7 @@
     'website': 'http://www.autodidactati.com/',
     'license': '',
     'depends': [
-        'base','contacts','ati_titulos','portal',
+        'base','contacts','portal',
         ],
     'data': [
         'views/menu.xml',
