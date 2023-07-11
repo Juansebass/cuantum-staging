3e642e26bf59e5da5ac40f311b5cb616350f1bfe
Juansebass <42044743+Juansebass@users.noreply.github.com>
2023-07-11 09:22:50 -0500

    Changes to acta adicion

--- ati_sale_custom/models/sale_order_line_inherit.py
+++ ati_sale_custom/models/sale_order_line_inherit.py
@@ -167,7 +167,9 @@ class SaleOrder(models.Model):
             'default_use_template': bool(template.id),
             'default_template_id': template.id,
             'default_composition_mode': 'comment',
-            'custom_layout': "mail.mail_notification_paynow",
+            'default_email_layout_xmlid': 'mail.mail_notification_layout_with_responsible_signature',
+            'proforma': self.env.context.get('proforma', False),
+            #'custom_layout': "mail.mail_notification_paynow",
             'force_email': True,
         }
 
