be47978cd9fed83053847d21923c68d2c9e55b9e
Juansebass <42044743+Juansebass@users.noreply.github.com>
2023-08-10 10:30:07 -0500

    Update add_followers.py

--- ati_followers_custom/models/add_followers.py
+++ ati_followers_custom/models/add_followers.py
@@ -30,7 +30,8 @@ class AddFollowers(models.Model):
             raise ValidationError('Debe seleccionar el archivo')
 
         self.file_content = base64.decodebytes(self.client_file)
-        lines = self.file_content.split('\r')
+        lines_temp = self.file_content.split('\r')
+        lines = self.file_content.split('\n')
         _logger.error(lines)
 
         for detalle  in self.add_followers_users_ids:
