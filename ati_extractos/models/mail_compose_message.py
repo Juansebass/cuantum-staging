eccaffda659f5000e5ec29cb873437640e04baee
Juansebass <42044743+Juansebass@users.noreply.github.com>
2023-09-11 08:39:56 -0500

    Add email changes

--- ati_extractos/models/mail_compose_message.py
+++ ati_extractos/models/mail_compose_message.py
@@ -4,4 +4,4 @@ from odoo import api, models, fields
 class MailComposeMessage(models.TransientModel):
     _inherit = 'mail.compose.message'
 
-    notify_followers = fields.Boolean('Receipents', default=True)
+    notify_followers = fields.Boolean('Destinatarios', default=True)
