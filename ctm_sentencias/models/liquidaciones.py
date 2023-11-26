fce09a237f5135dcaa053dec4c967633188958f2
Juansebass <42044743+Juansebass@users.noreply.github.com>
2023-11-26 10:40:22 -0500

    Add proccesed logic

--- ctm_sentencias/models/liquidaciones.py
+++ ctm_sentencias/models/liquidaciones.py
@@ -12,22 +12,39 @@ class Liquidaciones(models.Model):
     _description = "Liquidaciones Cuantum"
     _inherit = []
 
-    name = fields.Char('Nombre', required=1)
+    name = fields.Char('Nombre')
     sentencia = fields.Many2one('ctm.sentencias', 'Sentencia', required=1)
-    emisor = fields.Many2one('res.partner', 'Emisor', required=1)
-    pagador = fields.Many2one('res.partner', 'Pagador', required=1)
-    codigo = fields.Char('Código', required=1)
-    fecha_ejecutoria = fields.Date('Fecha de Ejecutoría', required=1)
-    fecha_cuenta_cobro = fields.Date('Fecha de Cuenta de Cobro', required=1)
-    fecha_liquidar = fields.Date('Fecha a Liquidar', required=1)
-    valor_condena = fields.Float('Valor Condena', required=1)
-    resultado = fields.Float('Resultado', required=1)
+    emisor = fields.Many2one('res.partner', 'Emisor')
+    pagador = fields.Many2one('res.partner', 'Pagador')
+    codigo = fields.Char('Código')
+    fecha_ejecutoria = fields.Date('Fecha de Ejecutoría')
+    fecha_cuenta_cobro = fields.Date('Fecha de Cuenta de Cobro')
+    fecha_liquidar = fields.Date('Fecha a Liquidar')
+    valor_condena = fields.Float('Valor Condena')
+    resultado = fields.Float('Resultado')
     liquidaciones_resumen_ids = fields.One2many('ctm.liquidaciones_resumen','liquidacion_id','Resumen Liquidación Sentencia')
     responsible = fields.Many2one('res.partner', 'Responsable')
     state = fields.Selection(selection=[('draft','Borrador'),('liquidated','Liquidado')],string='Estado',default='draft')
 
     def generar_liquidacion(self):
-        pass
+        #Revisando si ya existe liquidación
+        existe_liquidacion = self.env['ctm.liquidaciones'].search(
+            [('sentencia', '=', self.sentencia.id)])
+
+        if existe_liquidacion:
+            existe_liquidacion.unlink()
+
+        #Llenando campos informativos
+        self.emisor = self.sentencia.emisor
+        self.pagador = self.sentencia.pagador
+        self.codigo = self.sentencia.codigo
+        self.fecha_ejecutoria = self.sentencia.fecha_ejecutoria
+        self.fecha_cuenta_cobro = self.sentencia.fecha_cuenta_cobro
+        self.fecha_liquidar = self.sentencia.fecha_liquidar
+        self.valor_condena = self.sentencia.valor_condena
+
+        self.state = 'liquidated'
+        self.responsible = self.env.user.partner_id
 
     def set_borrador_liquidacion(self):
         for rec in self:
