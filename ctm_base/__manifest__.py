01af5c78439d6dadd519c647dce6ae0399d90ee7
Juansebass <42044743+Juansebass@users.noreply.github.com>
2024-01-26 18:11:38 -0500

    Add ctm base

---
+++ ctm_base/__manifest__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Base Cuantum',
+    'category': 'Base',
+    'summary': 'Modulo BAse  Cuantum',
+    'version': '15',
+    'description': """ """,
+    'author': 'Juan Sebastian Correa Acevedo',
+    'license': '',
+    'depends': [
+        ],
+    'data': [
+        'security/ir.model.access.csv',
+    ],
+    'installable': True,
+    'auto_install': False
+}
