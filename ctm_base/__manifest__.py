208d425991a9482a99ee2893df4d508818a452f1
Juansebass <42044743+Juansebass@users.noreply.github.com>
2024-03-22 14:31:03 -0500

    Update __manifest__.py

--- ctm_base/__manifest__.py
+++ ctm_base/__manifest__.py
@@ -13,7 +13,8 @@
         ],
     'data': [
         'security/cuantum_security.xml',
-        'views/contact_views.xml'
+        'views/contact_views.xml',
+        'views/menu_views.xml',
     ],
     'installable': True,
     'auto_install': False
