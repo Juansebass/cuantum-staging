f317dab22e8881f72ef5f09b307105ba73273787
Juansebass <42044743+Juansebass@users.noreply.github.com>
2024-03-23 08:45:57 -0500

    Update sentencias.py

--- ctm_sentencias/models/sentencias.py
+++ ctm_sentencias/models/sentencias.py
@@ -16,7 +16,24 @@ class Sentencias(models.Model):
     emisor = fields.Many2one('res.partner','Emisor',required=1)
     pagador = fields.Many2one('res.partner','Pagador',required=1)
     codigo = fields.Char('Código', required=1)
-    statum = fields.Integer('Statum')
+    statum = fields.Selection(
+        string='Statum',
+        selection=[
+            ('CSF', 'CSF'),
+            ('1', '1'),
+            ('2', '2'),
+            ('3', '3'),
+            ('4', '4'),
+            ('5', '5'),
+            ('6', '6'),
+            ('7', '7'),
+            ('8', '8'),
+            ('9', '9'),
+
+        ],
+        default='CSF',
+        required=True
+    )
     fecha_ejecutoria = fields.Date('Fecha de Ejecutoría', required=1)
     fecha_cuenta_cobro = fields.Date('Fecha de Cuenta de Cobro', required=1)
     fecha_liquidar = fields.Date('Fecha a Liquidar', required=1)
