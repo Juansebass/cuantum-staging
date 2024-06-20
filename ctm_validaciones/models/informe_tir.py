06b3bf19e56d75c3c6c08d3787d406f3feada1b6
Juansebass <42044743+Juansebass@users.noreply.github.com>
2024-06-20 11:28:51 -0500

    Add reporte tir por gestor

--- ctm_validaciones/models/informe_tir.py
+++ ctm_validaciones/models/informe_tir.py
@@ -54,6 +54,7 @@ class InformeTIR(models.Model):
         ])
 
         for extracto in extractos:
+            #Extracto totales
             self.env['informe.detalle.tir'].create({
                 'informe_tir_id': self.id,
                 'cliente': extracto.cliente.id,
@@ -62,9 +63,66 @@ class InformeTIR(models.Model):
                 'tir_semestral': extracto.tir_semestral,
                 'tir_anual': extracto.tir_anual,
             })
+            #Extracto por gestores
+            #CSF
+            gestor = self.env['ati.gestor'].search([('code', '=', 'CUANTUM')])
+            for tipo_inv in ['FAC', 'LIB', 'SEN', 'MUT']:
+                tipo = self.env['ati.investment.type'].search([('code', '=', tipo_inv)])
+                if tipo == 'FAC':
+                    mensual = extracto.cuantum_fac_mensual
+                    trimestral = extracto.cuantum_fac_trimestral
+                    semestral = extracto.cuantum_fac_semestral
+                    anual = extracto.cuantum_fac_anual
+                elif tipo == 'LIB':
+                    mensual = extracto.cuantum_lib_mensual
+                    trimestral = extracto.cuantum_lib_trimestral
+                    semestral = extracto.cuantum_lib_semestral
+                    anual = extracto.cuantum_lib_anual
+                elif tipo == 'SEN':
+                    mensual = extracto.cuantum_sen_mensual
+                    trimestral = extracto.cuantum_sen_trimestral
+                    semestral = extracto.cuantum_sen_semestral
+                    anual = extracto.cuantum_sen_anual
+                elif tipo == 'MUT':
+                    mensual = extracto.cuantum_mut_mensual
+                    trimestral = extracto.cuantum_mut_trimestral
+                    semestral = extracto.cuantum_mut_semestral
+                    anual = extracto.cuantum_mut_anual
+                self.create_record_tir_gestor(extracto, gestor, tipo, mensual, trimestral, semestral, anual)
+
+            #FCL
+            gestor = self.env['ati.gestor'].search([('code', '=', 'FCL')])
+            tipo = self.env['ati.investment.type'].search([('code', '=', 'LIB')])
+            mensual = extracto.fcl_lib_mensual
+            trimestral = extracto.fcl_lib_trimestral
+            semestral = extracto.fcl_lib_semestral
+            anual = extracto.fcl_lib_anual
+            self.create_record_tir_gestor(extracto, gestor, tipo, mensual, trimestral, semestral, anual)
+
+            #FCP
+            gestor = self.env['ati.gestor'].search([('code', '=', 'FCP')])
+            tipo = self.env['ati.investment.type'].search([('code', '=', 'SEN')])
+            mensual = extracto.fcp_sen_mensual
+            trimestral = extracto.fcp_sen_trimestral
+            semestral = extracto.fcp_sen_semestral
+            anual = extracto.fcp_sen_anual
+            self.create_record_tir_gestor(extracto, gestor, tipo, mensual, trimestral, semestral, anual)
 
         self.responsible = self.env.user.partner_id
         self.state = 'processed'
+
+    def create_record_tir_gestor(self, extracto, gestor, tipo, mensual, trimestral, semestral, anual):
+        self.env['informe.detalle.tir'].create({
+            'informe_tir_id': self.id,
+            'cliente': extracto.cliente.id,
+            'gestor_id': gestor.id,
+            'tipo_id': tipo.id,
+            'tir_mensual': mensual,
+            'tir_trimestral': trimestral,
+            'tir_semestral': semestral,
+            'tir_anual': anual,
+        })
+
 
     def action_exportar_xls(self):
         output = io.BytesIO()
@@ -74,10 +132,12 @@ class InformeTIR(models.Model):
 
         # Write headers
         sheet.write(0, 0, 'Cliente')
-        sheet.write(0, 1, 'TIR Mensual')
-        sheet.write(0, 2, 'TIR Trimestral')
-        sheet.write(0, 3, 'TIR Semestral')
-        sheet.write(0, 4, 'TIR Anual')
+        sheet.write(0, 1, 'Gestor')
+        sheet.write(0, 2, 'Tipo')
+        sheet.write(0, 3, 'TIR Mensual')
+        sheet.write(0, 4, 'TIR Trimestral')
+        sheet.write(0, 5, 'TIR Semestral')
+        sheet.write(0, 6, 'TIR Anual')
 
         sheet.set_column(0, 0, 50)
         sheet.set_column(1, 10, 20)
@@ -86,10 +146,12 @@ class InformeTIR(models.Model):
         row = 1
         for detalle in self.detalle_tir_ids:
             sheet.write(row, 0, detalle.cliente.name)
-            sheet.write(row, 1, detalle.tir_mensual / 100, money)
-            sheet.write(row, 2, detalle.tir_trimestral / 100, money)
-            sheet.write(row, 3, detalle.tir_semestral / 100, money)
-            sheet.write(row, 4, detalle.tir_anual / 100, money)
+            sheet.write(row, 1, detalle.gestor_id.name)
+            sheet.write(row, 2, detalle.tipo_id.name)
+            sheet.write(row, 3, detalle.tir_mensual / 100, money)
+            sheet.write(row, 4, detalle.tir_trimestral / 100, money)
+            sheet.write(row, 5, detalle.tir_semestral / 100, money)
+            sheet.write(row, 6, detalle.tir_anual / 100, money)
             row += 1
 
         workbook.close()
@@ -131,6 +193,8 @@ class DetalleTIR(models.Model):
 
     informe_tir_id = fields.Many2one('informe.tir', string='Informe TIR')
     cliente = fields.Many2one('res.partner', 'Cliente')
+    gestor_id = fields.Many2one('ati.gestor', 'Gestor')
+    tipo_id = fields.Many2one('ati.investment.type', 'Tipo')
     tir_mensual = fields.Float('TIR Mensual')
     tir_trimestral = fields.Float('TIR Trimestral')
     tir_semestral = fields.Float('TIR Semestral')
