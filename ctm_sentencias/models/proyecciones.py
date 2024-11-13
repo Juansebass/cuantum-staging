5044dab397d2d3501d9102ada67f3e1cca9ef1e7
Juansebass <42044743+Juansebass@users.noreply.github.com>
2024-11-12 20:37:21 -0500

    Update proyecciones.py

--- ctm_sentencias/models/proyecciones.py
+++ ctm_sentencias/models/proyecciones.py
@@ -171,11 +171,17 @@ class Proyecciones(models.Model):
                 fechas.append((fecha_incial, fecha_final))
 
             for fecha in fechas:
+                 #Buscando tasas
+                tasa_conf = self.env['ctm.tasas'].search(
+                [('fecha_inicio', '<=', fecha), ('fecha_final', '>=', fecha)], limit=1)
+                if not tasa_conf:
+                    raise ValidationError('No hay una tasa configurada para la fecha {0}'.format(fecha))
                 self.env['ctm.proyeccion_venta'].create(
                     {
                         'proyeccion_id': record.id,
                         'fecha_inicial': fecha[0],
                         'fecha_final': fecha[1],
+                        'tasa': tasa_conf.usura,
                     }
                 )
           
