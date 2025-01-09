28bfa0cadcb6f114dd9f5c4d920df40b6168a669
Juan Sebastian Correa Acevedo <juanse@MacBook-Pro-de-Juan.local>
2025-01-09 07:21:20 -0500

    [ADD]New Fields to carg_sentencias

--- ctm_sentencias/models/cargar_sentencias.py
+++ ctm_sentencias/models/cargar_sentencias.py
@@ -1,9 +1,7 @@
-from odoo import models, fields, api
+from odoo import models, fields
 from odoo.exceptions import ValidationError
 import base64
 from datetime import datetime
-
-
 
 
 class CargarSentencias(models.Model):
@@ -16,14 +14,20 @@ class CargarSentencias(models.Model):
     delimiter = fields.Char('Delimitador', default=";")
     fch_procesado = fields.Datetime('Fecha procesado')
     responsable = fields.Many2one('res.partner', 'Responsable de proceso')
-    state = fields.Selection(selection=[('draft', 'Borrador'), ('processed', 'Procesado')], string='Estado',
-                             default='draft')
+    state = fields.Selection(
+        selection=[('draft', 'Borrador'), ('processed', 'Procesado')],
+        string='Estado',
+        default='draft'
+    )
     file_content = fields.Text('Texto archivo')
     not_processed_content = fields.Text('Texto no procesado')
     clientes_creados = fields.Text('Creados')
     skip_first_line = fields.Boolean('Saltear primera linea', default=True)
-    client_match = fields.Selection(selection=[('name', 'Nombre')], string='Buscar clientes por...', default='name')
-
+    client_match = fields.Selection(
+        selection=[('name', 'Nombre')],
+        string='Buscar clientes por...',
+        default='name'
+    )
 
     def btn_process(self):
         _procesados = ""
@@ -31,7 +35,9 @@ class CargarSentencias(models.Model):
         vals = {}
         self.ensure_one()
         if not self.client_match:
-            raise ValidationError('Debe seleccionar metodo de busqueda de clientes')
+            raise ValidationError(
+                'Debe seleccionar metodo de busqueda de clientes'
+            )
         if not self.delimiter:
             raise ValidationError('Debe ingresar el delimitador')
         if not self.client_file:
@@ -39,12 +45,11 @@ class CargarSentencias(models.Model):
         if self.state != 'draft':
             raise ValidationError('Archivo procesado!')
 
-
         self.file_content = base64.decodebytes(self.client_file)
-        lines =  self.file_content.replace('\n', '')
+        lines = self.file_content.replace('\n', '')
         lines = lines.split('\r')
 
-        for i,line in enumerate(lines):
+        for i, line in enumerate(lines):
             new_record = None
             if self.skip_first_line and i == 0:
                 continue
@@ -60,7 +65,7 @@ class CargarSentencias(models.Model):
                     codigo = lista[3]
                     statum = lista[4]
                     fecha_ejecutoria = lista[5]
-                    fecha_cuenta_cobro= lista[6]
+                    fecha_cuenta_cobro = lista[6]
                     fecha_liquidar = lista[7]
                     valor_condena = lista[8]
                     nit_fcp_statum = lista[9]
@@ -71,16 +76,27 @@ class CargarSentencias(models.Model):
                     valor_giro = lista[14]
                     comision = lista[15]
                     costas = lista[16]
-
+                    retencion_total = lista[17]
+                    estructuracion = lista[18]
+                    intermediacion = lista[19]
+                    descuento_diluido = lista[20]
+                    comision_gestion_cuantum = lista[21]
+                    ingreso_anticipado_cuantum = lista[22]
+                    fecha_liquidar_neutral = lista[23]
+                    fecha_liquidar_optimista = lista[24]
+                    fecha_liquidar_acido = lista[25]
 
                     vals.clear()
 
                     emisor = self.env['res.partner'].search(
-                        [(self.client_match, '=', emisor), ('act_in', '=', 'activo')])
+                        [
+                            (self.client_match, '=', emisor),
+                            ('act_in', '=', 'activo')
+                        ])
                     if len(emisor) > 1:
                         raise ValidationError(
-                            "El CSV no se procesara por cliente con nombre repetido en sistema. El nombre {0} lo tienen dos o mas clientes".format(
-                                emisor))
+                            "El CSV no se procesara por cliente con nombre repetido en sistema. "
+                            "El nombre {0} lo tienen dos o mas clientes".format(emisor))
                     elif len(emisor) == 0:
                         raise ValidationError(
                             "El CSV no se procesara porque no se encuentra emisor o no está vinculado".format(
@@ -106,6 +122,7 @@ class CargarSentencias(models.Model):
                         formated_valor_condena = valor_condena.replace('$','').replace(' ', '').replace('.', '').replace(',', '.').replace('-','')
                         formated_valor_giro = valor_giro.replace('$','').replace(' ', '').replace('.', '').replace(',', '.').replace('-','')
                         formated_comision = comision.replace('$','').replace(' ', '').replace('.', '').replace(',', '.').replace('-','')
+                        formated_estructuracion = estructuracion.replace('$','').replace(' ', '').replace('.', '').replace(',', '.').replace('-','')
                         vals = {
                             "name": titulo,
                             "emisor": emisor.id,
@@ -124,7 +141,23 @@ class CargarSentencias(models.Model):
                             "valor_giro": formated_valor_giro,
                             "comision": formated_comision,
                             "costas": costas,
-
+                            "retencion_total": retencion_total,
+                            "estructuracion": formated_estructuracion,
+                            "intermediacion": intermediacion,
+                            "descuento_diluido": descuento_diluido,
+                            "comision_gestion_cuantum":
+                                comision_gestion_cuantum,
+                            "ingreso_anticipado_cuantum":
+                                ingreso_anticipado_cuantum,
+                            "fecha_liquidar_neutral": datetime.strptime(
+                                fecha_liquidar_neutral, '%d/%m/%Y'
+                            ) if fecha_liquidar_neutral else None,
+                            "fecha_liquidar_optimista": datetime.strptime(
+                                fecha_liquidar_optimista, '%d/%m/%Y'
+                            ) if fecha_liquidar_optimista else None,
+                            "fecha_liquidar_acido": datetime.strptime(
+                                fecha_liquidar_acido, '%d/%m/%Y'
+                            ) if fecha_liquidar_acido else None
                         }
                         new_record = self.env['ctm.sentencias'].sudo().create(vals)
                         _procesados += "{0};{1};{2};{3}\n".format(titulo, emisor.name, pagador.name, new_record.id)
