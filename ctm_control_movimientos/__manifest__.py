0488c6d82cecd809da308aac46b75dac2564ca16
Juan Sebastian Correa Acevedo <juanse@MacBook-Pro-de-Juan.local>
2025-01-17 08:00:39 -0500

    [ADD]Cerrar movimientos flujos wizard

--- ctm_control_movimientos/__manifest__.py
+++ ctm_control_movimientos/__manifest__.py
@@ -17,7 +17,7 @@
         'views/aplicaciones_views.xml',
         'views/cargar_movimientos_views.xml',
         'views/flujos_views.xml',
-        'wizards/cerrar_movimientos_flujos_views.xml',
+        'wizards/cerrar_movimiento_flujo_wizard_views.xml',
     ],
     'installable': True,
     'application': True,
