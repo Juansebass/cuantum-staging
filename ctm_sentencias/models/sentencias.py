8862d244b27bed2995634de5c312f699707b3e1d
Juan Sebastian Correa Acevedo <juanse@MacBook-Pro-de-Juan.local>
2025-01-20 08:05:11 -0500

    [ADD]Validation for nit fcp

--- ctm_sentencias/models/sentencias.py
+++ ctm_sentencias/models/sentencias.py
@@ -76,6 +76,12 @@ class Sentencias(models.Model):
         if res.statum != 'CSF' and not res.nit_fcp_statum:
             raise ValidationError('Debe ingresar el NIT FCP STATUM')
         return res
+    
+    def write(self, vals):
+        res = super(Sentencias, self).write(vals)
+        if self.statum != 'CSF' and not self.nit_fcp_statum:
+            raise ValidationError('Debe ingresar el NIT FCP STATUM')
+        return res
 
     def generar_proyeccion(self):
         for record in self:
