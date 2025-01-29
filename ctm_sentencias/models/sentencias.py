7ee37a8a6f66bea055428eef4e5f083bdded87b1
Juan Sebastian Correa Acevedo <juanse@MacBook-Pro-de-Juan.local>
2025-01-29 07:42:41 -0500

    [FIX]Required fields for sentencias

--- ctm_sentencias/models/sentencias.py
+++ ctm_sentencias/models/sentencias.py
@@ -74,7 +74,7 @@ class Sentencias(models.Model):
     def create(self, vals):
         res = super(Sentencias, self).create(vals)
         if self.statum != 'CSF':
-            if not self.nit_fcp_statum:
+            if not vals.get('nit_fcp_statum', self.nit_fcp_statum):
                 raise ValidationError('Debe ingresar el NIT FCP STATUM')
             if not self.vendedor:
                 raise ValidationError('Debe ingresar el Vendedor')
