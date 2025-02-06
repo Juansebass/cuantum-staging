c8e19f07a1e0cc368101253ca114e2e72e68c323
Juan Sebastian Correa Acevedo <juanse@MacBook-Pro-de-Juan.local>
2025-02-06 08:12:28 -0500

    [ADD]Recalculate movimientos flujos

--- ctm_control_movimientos/models/flujos.py
+++ ctm_control_movimientos/models/flujos.py
@@ -72,7 +72,7 @@ class Flujos(models.Model):
                 cdg = (((1 + self.cdg) ** (1 / 365)) - 1) * (fecha_final - fecha_inicial).days * past_valor_activo
                 pago_otros_conceptos = flujo.aplicacion_id.valor if flujo.aplicacion_id.valor > flujo.aplicacion_id.otros else flujo.aplicacion_id.otros
                 cdg_acumulado = past_movimiento_id.cdg + cdg
-                pago_cdg = cdg_acumulado if self.valor - pago_otros_conceptos > cdg_acumulado else self.valor - pago_otros_conceptos
+                pago_cdg = cdg_acumulado if flujo.aplicacion_id.valor - pago_otros_conceptos > cdg_acumulado else flujo.aplicacion_id.valor - pago_otros_conceptos
                 pago_rendimientos = rendimiento_acumulado if flujo.aplicacion_id.valor - pago_otros_conceptos - pago_cdg > rendimiento_acumulado else flujo.aplicacion_id.valor - pago_otros_conceptos - pago_cdg
                 pago_capital = flujo.aplicacion_id.valor - pago_otros_conceptos - pago_cdg - pago_rendimientos
                 flujo.write({
