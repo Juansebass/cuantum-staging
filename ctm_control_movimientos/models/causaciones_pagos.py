9d5a293e9ab803729d7a6cdb8ee2534814aa56fc
Juan Sebastian Correa Acevedo <juanse@MacBook-Pro-de-Juan.local>
2025-02-18 08:05:42 -0500

    [ADD]Causaciones y pagos

--- ctm_control_movimientos/models/causaciones_pagos.py
+++ ctm_control_movimientos/models/causaciones_pagos.py
@@ -40,7 +40,7 @@ class CausacionesPagos(models.Model):
     @api.model
     def write(self, var):
         res = super(CausacionesPagos, self).write(var)
-        res.name = f'{res.fecha_unica.strftime("%Y-%m-%d")} - {res.gestor_id.name} - {res.investment_type_id.name}'
+        res.name = f'{self.fecha_unica.strftime("%Y-%m-%d")} - {self.gestor_id.name} - {self.investment_type_id.name}'
         return res
 
     def action_cargar_clientes(self):
