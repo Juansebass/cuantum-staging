eeb9ecab55ce35530fc7706712345ff2088c2060
Juan Sebastian Correa Acevedo <juanse@MacBook-Pro-de-Juan.local>
2025-02-21 07:43:43 -0500

    [ADD]Descarga zip

--- ctm_control_movimientos/wizards/certificado_recaudos_wizard.py
+++ ctm_control_movimientos/wizards/certificado_recaudos_wizard.py
@@ -22,7 +22,7 @@ class CertificadoRecaudosWizard(models.TransientModel):
             ('investment_type_id', '=', self.investment_type_id.id),
         ])
 
-        pdf_content, _ = self.env.ref('ctm_control_movimientos.certificado_recaudos_template')._render_qweb_pdf(movimientos_flujos.ids)
+        pdf_content, _ = self.env['ir.actions.report']._render_qweb_pdf('ctm_control_movimientos.certificado_recaudos_template', movimientos_flujos.ids)
         zip_buffer = io.BytesIO()
         with zipfile.ZipFile(zip_buffer, 'w', zipfile.ZIP_DEFLATED) as zip_file:
 
