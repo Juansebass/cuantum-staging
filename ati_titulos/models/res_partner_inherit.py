ec7b63ff4dd15b07919f4e337af2f8f7c00139a0
Juan Sebastian Correa Acevedo <juanse@MacBook-Pro-de-Juan.local>
2025-03-27 07:58:42 -0500

    [UPDATE]Calculos rendimientos

--- ati_titulos/models/res_partner_inherit.py
+++ ati_titulos/models/res_partner_inherit.py
@@ -204,7 +204,7 @@ class ResPartner(models.Model):
                     move.saldo = previous_saldo + move.value
                 if previous_date:
                     move.calculo_rendimiento = previous_saldo * (
-                        ((1 + (rec.tasa_rendimiento_csf) / 100) ** (1 / 365)) - 1
+                        ((1 + (rec.tasa_rendimiento_csf / 100)) ** (1 / 365)) - 1
                     ) * (move.date - previous_date).days
                 previous_date = move.date
                 previous_saldo = move.saldo
