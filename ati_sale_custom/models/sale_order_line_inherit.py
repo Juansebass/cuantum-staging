d829ae03a851cb83b0b3a6e4da61a590f82fe0c0
Juan Sebastian Correa Acevedo <juanse@MacBook-Pro-de-Juan.local>
2025-05-05 07:59:11 -0500

    [ADD]Agregar linea compra rpr con ofertas

--- ati_sale_custom/models/sale_order_line_inherit.py
+++ ati_sale_custom/models/sale_order_line_inherit.py
@@ -1,12 +1,13 @@
 # -*- coding: utf-8 -*-
-from odoo import fields, models, api, _
-from odoo.exceptions import ValidationError
+from odoo import fields, models, api  # type: ignore
+from odoo.exceptions import ValidationError  # type: ignore
 import logging
 import datetime
 import locale
 import base64
 
 _logger = logging.getLogger(__name__)
+
 
 class SaleOrder(models.Model):
     _inherit = "sale.order"
@@ -21,9 +22,9 @@ class SaleOrder(models.Model):
         required=True
     )
 
-    #Variable utilizadas para cargar ofertas de titulos segun gestor y tipo de inversion
-    gestor_ofertar = fields.Many2one('ati.gestor','Gestor de titulos')
-    tipo_producto_ofertar = fields.Many2one('ati.investment.type','Tipo de producto')
+    # Variable utilizadas para cargar ofertas de titulos segun gestor y tipo de inversion
+    gestor_ofertar = fields.Many2one('ati.gestor', 'Gestor de titulos')
+    tipo_producto_ofertar = fields.Many2one('ati.investment.type', 'Tipo de producto')
     fecha_celebracion = fields.Datetime(string='Fecha de Celebración', index=True,
                                         states={'draft': [('readonly', False)], 'sent': [('readonly', False)]},
                                         copy=False, default=fields.Datetime.now)
@@ -33,8 +34,8 @@ class SaleOrder(models.Model):
         ('no_calificada', 'No Calificada'),
         ('aceptada', 'Aceptada'),
         ('rechazada', 'Rechazada'),
-        ],
-        string = 'Estado Oferta FCL', default = 'no_calificada'
+    ],
+        string='Estado Oferta FCL', default='no_calificada'
     )
     asignado = fields.Boolean('Asignado', default=False)
 
@@ -83,19 +84,19 @@ class SaleOrder(models.Model):
             # Verificamos que existan ofertas, en el caso de existir las agregamos a las lineas
             if len(ofertas) == 0:
                 raise ValidationError('El gestor {0} no tiene disponible Titulos de tipo {1}'.format(rec.gestor_ofertar.name, rec.tipo_producto_ofertar.name))
-            
+
             producto = self.env['product.product']
             if rec.tipo_producto_ofertar.code == 'FAC':
-                producto = self.env['product.product'].search([('name','=','Factoring')])
+                producto = self.env['product.product'].search([('name', '=', 'Factoring')])
             elif rec.tipo_producto_ofertar.code == 'LIB':
-                producto = self.env['product.product'].search([('name','=','Libranzas')])
+                producto = self.env['product.product'].search([('name', '=', 'Libranzas')])
             elif rec.tipo_producto_ofertar.code == 'SEN':
-                producto = self.env['product.product'].search([('name','=','Sentencias')])
+                producto = self.env['product.product'].search([('name', '=', 'Sentencias')])
             elif rec.tipo_producto_ofertar.code == 'MUT':
-                producto = self.env['product.product'].search([('name','=','Mutuos')])
-            
+                producto = self.env['product.product'].search([('name', '=', 'Mutuos')])
+
             for o in ofertas:
-                rec.order_line = [(0,0,{
+                rec.order_line = [(0, 0, {
                     'emisor_line' : o.issuing.id,
                     'pagador_line' : o.payer.id,
                     'plazo_pago' : o.redemption_date,
@@ -107,12 +108,12 @@ class SaleOrder(models.Model):
                     'price_unit' : o.value,
                     'product_id' : producto.id
                 })]
-            
+
     def action_confirm(self):
         res = super(SaleOrder, self).action_confirm()
         for rec in self:
-            
-            #Recorremos todas las lineas para verificar si hay alguna linea asociada a un titulo de oferta (ati.titulo.oferta) y si las hay consultamos que ya no esten adquiridas
+
+            # Recorremos todas las lineas para verificar si hay alguna linea asociada a un titulo de oferta (ati.titulo.oferta) y si las hay consultamos que ya no esten adquiridas
             for ol in rec.order_line:
                 if len(ol.titulo_oferta) > 0:
                     if ol.titulo_oferta.odquirido:
@@ -122,6 +123,31 @@ class SaleOrder(models.Model):
                         ol.titulo_oferta.cliente = rec.partner_id.id
                 # Agregando fecha de celebración a la orden
                 self.date_order = self.fecha_celebracion
+            compra_type = self.env['ati.movement.type'].search([('code', '=', 'COMPRA')])
+            if rec.gestor_ofertar.code == 'FCL':
+                self.env['ati.recurso.recompra.fcl'].create({
+                    'date': self.fecha_celebracion,
+                    'value': rec.amount_total,
+                    'investment_type': rec.tipo_producto_ofertar.id,
+                    'movement_type': compra_type,
+                    'buyer': rec.partner_id.id,
+                })
+            if rec.gestor_ofertar.code == 'CUANTUM':
+                self.env['ati.recurso.recompra.csf'].create({
+                    'date': self.fecha_celebracion,
+                    'value': rec.amount_total,
+                    'investment_type': rec.tipo_producto_ofertar.id,
+                    'movement_type': compra_type,
+                    'buyer': rec.partner_id.id,
+                })
+            if rec.gestor_ofertar.code == 'FCP':
+                self.env['ati.recurso.recompra.fcp'].create({
+                    'date': self.fecha_celebracion,
+                    'value': rec.amount_total,
+                    'investment_type': rec.tipo_producto_ofertar.id,
+                    'movement_type': compra_type,
+                    'buyer': rec.partner_id.id,
+                })
         return res
 
     def action_assign(self):
@@ -132,7 +158,6 @@ class SaleOrder(models.Model):
                         ol.titulo_oferta.cliente = rec.partner_id.id
             self.asignado = True
 
-
     # Se verifica si los productos de la oferta son distintos, de ser asi se avisa que solo se puede tener un solo tipo y se cancela la accion
     @api.onchange('order_line')
     def on_change_order_line_restriccion_producto(self):
@@ -141,7 +166,6 @@ class SaleOrder(models.Model):
                 for ol in rec.order_line:
                     if ol.product_id.id != rec.order_line[0].product_id.id:
                         raise ValidationError('Solo puedes ofrecer productos del mismo tipo en una Oferta')
-
 
     def get_group_results_fcl(self):
         for rec in self:
@@ -158,7 +182,7 @@ class SaleOrder(models.Model):
                 'avista_valor': avista_valor if avista_valor else 0,
                 'mes_letra': mes_actual.upper(),
             }
-            #Agregando totales
+            # Agregando totales
             total_valor = sum([results['coinvercop_valor'], results['soinpro_valor'], results['avista_valor']])
             results['total_valor'] = total_valor
             results['total_letra'] = self.currency_id.amount_to_text(total_valor).upper()
@@ -188,8 +212,8 @@ class SaleOrder(models.Model):
             'default_use_template': bool(template.id),
             'default_template_id': template.id,
             'default_composition_mode': 'comment',
-            #'default_email_layout_xmlid': 'mail.mail_notification_layout_with_responsible_signature',
-            #'proforma': self.env.context.get('proforma', False),
+            # 'default_email_layout_xmlid': 'mail.mail_notification_layout_with_responsible_signature',
+            # 'proforma': self.env.context.get('proforma', False),
             'custom_layout': "mail.mail_notification_light",
             'mark_so_as_sent': True,
             'force_email': True,
@@ -210,11 +234,11 @@ class SaleOrder(models.Model):
 class SaleOrderLine(models.Model):
     _inherit = "sale.order.line"
 
-    emisor_line = fields.Many2one('res.partner','Emisor')
-    pagador_line = fields.Many2one('res.partner','Pagador')
+    emisor_line = fields.Many2one('res.partner', 'Emisor')
+    pagador_line = fields.Many2one('res.partner', 'Pagador')
     plazo_pago = fields.Date('Plazo Estimado')
     taza_retorno = fields.Float('Tasa Estimada')
-    gestor_line = fields.Many2one('ati.gestor','Gestor')
+    gestor_line = fields.Many2one('ati.gestor', 'Gestor')
     n_titulo = fields.Char('Nº Titulo')
     reserva = fields.Float('Reserva')
-    titulo_oferta = fields.Many2one('ati.titulo.oferta','Titulo oferta')
+    titulo_oferta = fields.Many2one('ati.titulo.oferta', 'Titulo oferta')
